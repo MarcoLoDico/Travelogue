7f2059b2d03e1c4f9d279fe81893325b6f8b7bbe
MarcoLoDico <marcolodico1@gmail.com>
2025-09-20 18:25:49 -0400

    Added visit table
    fix by adding users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_20_221726) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_20_222507) do
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.integer "kind"
@@ -26,5 +26,41 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_221726) do
     t.index ["parent_id"], name: "index_places_on_parent_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "place_id", null: false
+    t.date "visited_on"
+    t.datetime "arrived_at"
+    t.datetime "departed_at"
+    t.text "notes"
+    t.float "lat"
+    t.float "lon"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_visits_on_place_id"
+    t.index ["user_id"], name: "index_visits_on_user_id"
+  end
+
   add_foreign_key "places", "places", column: "parent_id"
+  add_foreign_key "sessions", "users"
+  add_foreign_key "visits", "places"
+  add_foreign_key "visits", "users"
 end
